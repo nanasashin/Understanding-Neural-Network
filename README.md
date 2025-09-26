46da93bdacce4d8fcbecd4f2f0785050f8673c52
NatsuShii <renzjoshuamalig10@gmail.com>
2025-09-26 09:53:40 +0800

    loss calculation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 My Practice and Implemantation of Neural Network
+
+Sources:
+- sentdex yt playlist ( Neural Network form Scratch )
--- codes/NeuralNetwork.py
+++ codes/NeuralNetwork.py
@@ -5,6 +5,7 @@ test_inputs = [
     [6, 8, 9, 10]
 ]
 
+
 class LayerDense:
     def __init__(self, n_input, n_neurons):
         self.weights = 0.10 * np.random.randn(n_input, n_neurons)
@@ -13,9 +14,43 @@ class LayerDense:
     def forward(self, inputs):
         return np.dot(inputs, self.weights) + self.biases
 
+
 class ActivationReLu:
     def forward(self, inputs):
         return np.maximum(0, inputs)
+    
 
-print(ActivationReLu().forward(LayerDense(4, 8).forward(test_inputs)))
+class AticationSoftmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        return probabilities
 
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clippped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_pred.shape) == 1:
+            correct_confidence = y_pred_clippped[range(samples), y_true]
+
+        elif len(y_pred.shape) == 2:
+            correct_confidence = np.sum(y_pred_clippped*y_true, axis=1)
+
+        negative_log_likelihood = -np.log(correct_confidence)
+        return negative_log_likelihood
+
+
+first_layer = ActivationReLu().forward(LayerDense(4, 8).forward(test_inputs))
+second_layer = AticationSoftmax().forward(first_layer)
+
+calculation_loss = Loss_CategoricalCrossEntropy().calculate(second_layer, [[0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 1, 0, 0]])
+
+print(calculation_loss)
